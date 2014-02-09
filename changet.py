94e4702d14baf5bf194db9d76ea7ac025da95fec
John Labelle <argentage@gmail.com>
2014-02-08 20:54:31 -0600

    add windows compatability

--- changet.py
+++ changet.py
@@ -5,7 +5,7 @@ import os
 
 r = requests.get("http://boards.4chan.org/%s/res/%s" % (sys.argv[1],sys.argv[2]))
 if r.status_code == 404:
-    print "Couldn't find thread!"
+    print("Couldn't find thread!")
     exit(-1)
 folderName = sys.argv[1] + '-' + sys.argv[2]
 try:
@@ -19,12 +19,12 @@ imgUrls = ['http:' + r['href'] for r in soup.findAll(class_ = "fileThumb")]
 template= "<img src='%s'><br>%d<br>"
 pfile = open("index.html","a")
 for n,image in enumerate(imgUrls):
-    print image
+    print(image)
     fname = image.split("/")[-1]
     if os.path.exists(fname):
         continue
     r = requests.get(image)
-    f  = open(fname,"w")
+    f  = open(fname,"wb")
     f.write(r.content)
     f.close()
     pfile.write(template % (fname,n))
